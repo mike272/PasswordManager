36004c0f69b58b6aeb7f593d3091ebae2f5d8b0f
Michal Babinski <babinmichal@gmail.com>
2021-05-11 18:57:59 +0400

    Add project files.

---
+++ page2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswordManager
+{
+    /// <summary>
+    /// Interaction logic for page2.xaml
+    /// </summary>
+    public partial class page2 : Page
+    {
+        public page2()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+
+        }
+    }
+}
